9f5db0ab7cfe2f9fe5990d83c6c5ca8bc33aa139
MykhailoVobolis <mvobolis@gmail.com>
2024-02-05 18:45:28 +0200

    gallery and form ready

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,36 +1,32 @@
 const feedbackForm = document.querySelector('form');
+const localStorageKey = 'feedback-form-state';
+const savedLocalStorageKey = localStorage.getItem(localStorageKey);
+const feedback = JSON.parse(savedLocalStorageKey) ?? {};
 const userEmail = feedbackForm.elements.email;
 const userMessage = feedbackForm.elements.message;
-const localStorageKey = 'feedback-form-state';
+
+// Підставлення даних з локального сховища при оновлені сторінки
+userEmail.value = feedback.email ?? '';
+userMessage.value = feedback.message ?? '';
 
 // Запис введених даних у локальне сховище
-const feedback = {};
-
 feedbackForm.addEventListener('input', recordLocalStorage);
+
 function recordLocalStorage(event) {
   feedback.email = feedbackForm.elements.email.value.trim();
-
   feedback.message = feedbackForm.elements.message.value.trim();
-
   localStorage.setItem(localStorageKey, JSON.stringify(feedback));
 }
 
-// Підставляння даних з локального сховища у відповідні елементи форми при оновленні сторінки
-const savedLocalStorageKey = localStorage.getItem(localStorageKey);
-const parsedLocalStorageKey = JSON.parse(savedLocalStorageKey);
-
-userEmail.value = parsedLocalStorageKey.email ?? '';
-userMessage.value = parsedLocalStorageKey.message ?? '';
-
-// Відправлення даних, очищення форми та локального сховища
+// Дія при натисканні кнопки submit
 feedbackForm.addEventListener('submit', sendingData);
+
 function sendingData(event) {
   event.preventDefault();
-  if (
-    feedbackForm.elements.email.value !== '' &&
-    feedbackForm.elements.message.value !== ''
-  ) {
-    console.log(parsedLocalStorageKey);
+  if (!userEmail.value || !userMessage.value) {
+    return alert('Будь ласка, заповніть всі поля форми');
+  } else {
+    console.log(feedback);
     localStorage.removeItem(localStorageKey);
     feedbackForm.reset();
   }
